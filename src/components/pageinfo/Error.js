558a0b57c7b7edab4df8a03e560b0acebd53e017
rose <rooserwl@gmail.com>
2023-05-03 20:37:21 +0545

    1. Form Validation 2. Error Generation 3. Regex Matching 4. Routing Changes

--- src/components/pageinfo/Error.js
+++ src/components/pageinfo/Error.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Error = ({children}) => {
+const Error = ({msg}) => {
   return (
     <div>
-      <p>{children}</p>
+      <p>{msg}</p>
     </div>
   )
 }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -4,37 +4,62 @@ import Button from './formdetails/Button'
 import context from '../context/maincontext'
 import Error from '../pageinfo/Error'
 const Login = () => {
-  const {setUsers,users,setLoggedIn}=useContext(context);
+  const {setUsers,users,setLoggedIn,checkSignUp,checkLogin}=useContext(context);
   const [loguser,setLoguser]=useState({
     Name:'',
     Password:''
   })
-  const [error,setError]=useState(false)
+  const [login,setLogin]=useState(true)
+  const [error,setError]=useState({})
   const [user,setUser]=useState({
     Name:'',
     Email:'',
     Password:''
   })
+  const [msg,setMsg]=useState({
+    err:'',
+    text:''
+  })
   const handleSignUp=(e)=>{
     e.preventDefault();
+    const abc=checkSignUp({...user})
+    if(abc.text==="Success")
+    {
+      setLogin(prev=>!prev)
+    }
+    setMsg({...abc})
+    setTimeout(()=>{
+      setMsg({
+    err:'',
+    text:''
+  })
+    },2000)
     setUsers(n=>{
       const save=[...n,user]
-      console.log(save)
+      
       return save
     })
+    
   }
-
   const handleLogin=(e)=>{
     e.preventDefault();
-    users.map(n=>{
-      if(n.Name===loguser.Name && n.Password===loguser.Password){
-        setLoggedIn(true)
-      }
-    })
+    
+    const abc=checkLogin(loguser.Name,loguser.Password);
+    
+    setError({...abc})
+    setTimeout(()=>{
+      setError({})
+    },1000)
   }
-  const [login,setLogin]=useState(true)
+
+ 
+  
+  
   return (
     <div >
+      <h1 className='flex justify-center text-5xl my-10'>Authentication</h1>
+      <span className='flex justify-center text-red-400 '>{error.text}</span>
+      <span className='flex justify-center text-red-400 '>{msg.text}</span>
         {login && <form action="get" className='pt-5 '>
             <Input name="Name" type="text" func={loguser.Name} set={setLoguser}/>
             
@@ -42,7 +67,6 @@ const Login = () => {
             <Button name='Log in' handle={handleLogin}/><br />
             <input type='button' className='setter' value="Create Account"onClick={(e)=>{e.preventDefault(); setLogin(false)}}></input>
         </form>}
-
         {!login && <form action="get" className='pt-5'>
             <Input name="Name" type="text" func={user.Name} set={setUser}/>
             <Input name="Email" type="email" func={user.Email} set={setUser}/>
@@ -51,6 +75,8 @@ const Login = () => {
             <input type='button' className='setter' value="Already Have Account"onClick={(e)=>{e.preventDefault(); setLogin(true)}}></input>
         </form>
         }
+        {msg.length===0?<Error msg/>:<div></div>}
+        
          
     </div>
   )
