2afac309311a58ca38966ac439bcce32adc7eeb4
rose <rooserwl@gmail.com>
2023-09-10 16:03:10 +0545

    integrating to netlify

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:3000/"
+    baseURL:"https://peach-chat.onrender.com/"
 })
 export default axiosInstance;
