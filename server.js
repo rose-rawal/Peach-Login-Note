3ab3cabe83ca9c0bf451bca05ce13a312b0ca533
rose <rooserwl@gmail.com>
2023-09-10 14:51:25 +0545

    updated

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/index.js';
 import cors from 'cors';
-import ServerlessHttp from 'serverless-http';
+// import ServerlessHttp from 'serverless-http';
+import http from 'http';
+import { Server as socketIO } from 'socket.io';
+
 const app=express();
+const server=http.createServer(app);
+const io=new socketIO(server);
+
+
 app.get('/',(req,res)=>{
     res.json({ok:true});
 })
@@ -17,6 +24,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/peach')
     console.log(err)
 })
 app.use('/',router);
-app.listen(3000,()=>{
-    console.log("listening in port 3000")
+// app.listen(3000,()=>{
+//     console.log("listening in port 3000")
+// })
+
+io.on('connection',(socket)=>{
+   console.log("user is connected");
+   
+    
+})
+server.listen(3000,()=>{
+    console.log("listening in server 3000");
 })
