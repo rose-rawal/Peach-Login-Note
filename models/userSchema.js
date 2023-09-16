5138f317193b8c22f6e9da8c5573b9f18ff01ee5
rose <rooserwl@gmail.com>
2023-09-16 11:57:38 +0545

    login signup connection with db

---
+++ models/userSchema.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const userSchema=mongoose.Schema({
+    Name:{
+        type:String,
+        required:true
+    },
+    Email:{
+        type:String,
+        required:true
+    },
+    Password:{
+        type:String,
+        required:true
+    }
+})
+export default mongoose.model('user',userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 
 import { Router } from 'express';
 import messageSchema from '../models/messageSchema.js';
-
+import userSchema from '../models/userSchema.js';
 
 const router=Router()
 async function seeAllMessage(req,res){
     const message= await messageSchema.find();
     return res.json(message);
 }
-router.get('/allUser',seeAllMessage);
+router.get('/allMessage',seeAllMessage);
 
 async function addMessage(req,res){
     try{
@@ -22,5 +22,23 @@ async function addMessage(req,res){
         return res.status(500).json({message:err});
     }
 }
-router.post('/addUser',addMessage)
+router.post('/addMessage',addMessage)
+
+async function seeAllUser(req,res){
+    const user=await userSchema.find();
+    return res.json(user);
+}
+router.get('/allUser',seeAllUser);
+
+async function addUser(req,res){
+    try{
+    const {Name,Email,Password}=req.body;
+    const added=await userSchema.create({Name,Email,Password});
+    return res.json(added);
+    }
+    catch(err){
+        return res.status(500).json({message:err})
+    }
+}
+router.post('/addUser',addUser);
 export default router
