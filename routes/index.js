1512a2f254b52564b1b59183cac22d44f50cf95b
rose <rooserwl@gmail.com>
2024-03-27 13:33:31 +0545

    bcrypt implemented

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,44 @@
-
-import { Router } from 'express';
-import messageSchema from '../models/messageSchema.js';
-import userSchema from '../models/userSchema.js';
-
-const router=Router()
-async function seeAllMessage(req,res){
-    const message= await messageSchema.find();
-    return res.json(message);
+import { Router } from "express";
+import messageSchema from "../models/messageSchema.js";
+import userSchema from "../models/userSchema.js";
+import bcrypt from "bcrypt";
+const router = Router();
+async function seeAllMessage(req, res) {
+  const message = await messageSchema.find();
+  return res.json(message);
 }
-router.get('/allMessage',seeAllMessage);
+router.get("/allMessage", seeAllMessage);
 
-async function addMessage(req,res){
-    try{
-    const {message,author}=req.body;
-    const added= await messageSchema.create({
-        message,author
-    })
-    return res.json(added)
-    }
-    catch(err){
-        return res.status(500).json({message:err});
-    }
-}
-router.post('/addMessage',addMessage)
-
-async function seeAllUser(req,res){
-    const user=await userSchema.find();
-    return res.json(user);
-}
-router.get('/allUser',seeAllUser);
-
-async function addUser(req,res){
-    try{
-    const {Name,Email,Password}=req.body;
-    const added=await userSchema.create({Name,Email,Password});
+async function addMessage(req, res) {
+  try {
+    const { message, author } = req.body;
+    const added = await messageSchema.create({
+      message,
+      author,
+    });
     return res.json(added);
-    }
-    catch(err){
-        return res.status(500).json({message:err})
-    }
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
 }
-router.post('/addUser',addUser);
-export default router
+router.post("/addMessage", addMessage);
+
+async function seeAllUser(req, res) {
+  const user = await userSchema.find();
+
+  return res.json(user);
+}
+router.get("/allUser", seeAllUser);
+
+async function addUser(req, res) {
+  try {
+    const { Name, Email, Password } = req.body;
+    const hash = await bcrypt.hash(Password, 10);
+    const added = await userSchema.create({ Name, Email, Password: hash });
+    return res.json(added);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+}
+router.post("/addUser", addUser);
+export default router;
